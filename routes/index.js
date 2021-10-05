8fb3bccb1be7e8d5f96b82e6270863b19575b39b
CreativeCodingGuy <90730649+CreativeCodingGuy@users.noreply.github.com>
2021-10-05 17:46:36 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET Home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET About Me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET My Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET My Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact Me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contacts' });
+});
+
+module.exports = router;
