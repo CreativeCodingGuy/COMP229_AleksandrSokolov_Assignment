06f9d0cb484532aea13b81d3b311352e8e8a262b
CreativeCodingGuy <90730649+CreativeCodingGuy@users.noreply.github.com>
2021-10-06 19:09:13 -0400

    Add comments and update the button Send on the Contact Me Page

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,3 +1,10 @@
+/*
+    File Name: app.js
+    Full Name: Aleksandr Sokolov
+    StudentID: 301139655
+    Date: 2021-10-06
+*/
+
 // IIFE - Immediately Invoked Function Expression
 (function() {
 
