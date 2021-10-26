11ad4b1a132f049312f766713f9e3304c07cf37e
CreativeCodingGuy <90730649+CreativeCodingGuy@users.noreply.github.com>
2021-10-26 19:31:38 -0400

    ain

--- controllers/contact.js
+++ controllers/contact.js
@@ -12,7 +12,7 @@ module.exports.displayContactList = (req, res, next) => {
         if (err) {
             return console.error(err);
         } else {
-            res.render('contact/list', { title: 'Contacts List', ContactList: contactList });
+            res.render('contact/list', { title: 'Contacts List', ContactList: contactList , displayName: req.user ? req.user.displayName : ''});
         }
     });
 
@@ -20,7 +20,7 @@ module.exports.displayContactList = (req, res, next) => {
 
 /* GET Route for displaying the Add page - CREATE operation. */
 module.exports.displayAddPage = (req, res, next) => {
-    res.render('contact/add', {title: 'Add Contact'});
+    res.render('contact/add', {title: 'Add Contact', displayName: req.user ? req.user.displayName : ''});
 };
 
 /* POST Route for processing the Add page - CREATE operation. */
@@ -52,7 +52,7 @@ module.exports.displayEditPage = (req, res, next) => {
             res.end(err);    
         } else {
             //show the edit view
-            res.render('contact/edit', {title: 'Edit Contact', contact: contactToEdit})
+            res.render('contact/edit', {title: 'Edit Contact', contact: contactToEdit, displayName: req.user ? req.user.displayName : ''})
         }
     })
 };
