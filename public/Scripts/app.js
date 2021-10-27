794fb8a95fae6508fc79f29d02b7a63603f59124
CreativeCodingGuy <90730649+CreativeCodingGuy@users.noreply.github.com>
2021-10-26 20:43:24 -0400

    Add the automatic sorting

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -2,7 +2,7 @@
     File Name: app.js
     Full Name: Aleksandr Sokolov
     StudentID: 301139655
-    Date: 2021-10-06
+    Date: 2021-10-26
 */
 
 // IIFE - Immediately Invoked Function Expression
@@ -12,7 +12,6 @@
         console.log("App Started...");
 
         let deleteButtons = document.querySelectorAll('.btn-danger');
-
         for(button of deleteButtons) {
             button.addEventListener('click', (event) => {
                 if (!confirm("Are you sure?")){
@@ -26,3 +25,29 @@
 
     window.addEventListener("load", Start);
 })();
+
+/*This function was taken from the w3schools and updated to work on my webapp*/
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementsByClassName("table")[0];
+    switching = true;
+    while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 1; i < (rows.length - 1); i++) {
+    shouldSwitch = false;
+    x = rows[i].getElementsByTagName("td")[0];
+    y = rows[i + 1].getElementsByTagName("td")[0];
+    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+    shouldSwitch = true;
+    break;
+    }
+    }
+    if (shouldSwitch) {
+    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+    switching = true;
+    }
+    }
+} 
+
+window.addEventListener("load", sortTable);
